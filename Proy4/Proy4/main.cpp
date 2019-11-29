c017382539773267788d188f0b6e45ff86d4a56d
Martín Tamay <martintamay9@gmail.com>
2019-11-28 22:31:18 -0300

    se suben cambios en collider

--- Proy4/Proy4/main.cpp
+++ Proy4/Proy4/main.cpp
@@ -1,24 +1,46 @@
 #include "GL/freeglut.h"
 #include <iostream>
 #include <cmath>
+#include "vecmath/vecmath.h"
+#include "vecmath/mat.h"
+#include "vecmath/vec.h"
+#include "Cube.h"
+
 using namespace std;
 
-float zoom = 12.0;
+const double HUGE_DOUBLE = 1e100;
+const double RAY_EPSILON = 0.00001;
+const double NORMAL_EPSILON = 0.00001;
+
+float zoom = 9.0;
 float zoomSensibility = 0.2;
 float camspeed = 0.1;
 
 float camx = 0.0,	camy = 0.0, camz = zoom;
-float angx = 0.0,	angy = 0.0, angz = 0.0;
-float tx = 0.0,		ty = 1.0,	tz = 0.0;
+Vec3f rightVect(1.0f, 0.0f, 0.0f);
+Vec3f upVect(0.0f, 1.0f, 0.0f);
+Vec3f backVect(0.0f, 0.0f, 1.0f);
+Vec3f col(0.0f, 0.0f, 0.0f);
+Vec3f antCam(0.0f, 0.0f, 0.0f);
+Mat3f rotMatrix(Vec3f u, float ang);
 
-int width, height;
+int currPlayer = 1;
+int width = 720, height = 720;
+
+Cube c;
+
 
 void reshape(int w, int h);
+bool centerTest(int x, int y, int z);
 void drawCube();
 void keyboard(unsigned char key, int x, int y);
+float collideBox(Vec3f o, Vec3f d);
+float collideSphere(Vec3f o, Vec3f d);
+int* checkCollide(Vec3f d);
 
 void init(void) {
 	glClearColor(0, 0, 0, 0);
+	glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
 	glEnable(GL_DEPTH_TEST);
 	glShadeModel(GL_SMOOTH);
 }
@@ -34,22 +56,41 @@ void display(void) {
 		0.0, 0.0, 0.0,
 		//orientación de la cámara (para que pueda mirar de cabeza o de costado)
 		//0.0, 0.0, 1.0
-		tx, ty, tz
+		upVect[0], upVect[1], upVect[2]
 	);//sitúa el punto de vista respecto al punto de atención
-	/* test de camara 
-	glColor3f(1.0, 1.0, 1.0);
+	/* test de camara */
+
+
+	drawCube();
+	glColor3f(1.0, 0.5, 1.0);
 	glBegin(GL_LINES);
 	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(tx, ty, tz);
+	glVertex3f(upVect[0], upVect[1], upVect[2]);
+	glEnd();
+	glColor3f(1.0, 1.0, 0.5);
+	glBegin(GL_LINES);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(rightVect[0], rightVect[1], rightVect[2]);
+	glEnd();
+	glColor3f(0.5, 1.0, 1.0);
+	glBegin(GL_LINES);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(backVect[0], backVect[1], backVect[2]);
 	glEnd();
 
-	glColor3f(0.0, 1.0, 0.0);
+	// el clic
+	glColor3f(1.0, 0.0, 0.0);
 	glBegin(GL_LINES);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(camx, camy, camz);
+	glVertex3f(0.0, 0.0, 0.0);
+	glVertex3f(col[0], col[1], col[2]);
 	glEnd();
-	*/
-	drawCube();
+
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_LINES);
+	glVertex3f(antCam[0], antCam[1], antCam[2]);
+	glVertex3f(antCam[0] + col[0] * 100, antCam[1] + col[1] * 100, antCam[2] + col[2] * 100);
+	glEnd();
+
 
 	glFlush();
 	glutSwapBuffers();
@@ -58,36 +99,188 @@ void display(void) {
 
 void mouseFunc(int button, int state, int coor_x, int coor_y) {
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
-		float x = ((float)coor_x / (float)width) * 2.0 - 1.0;
-		float y = ((float)coor_y / (float)height) * 2.0 - 1.0;
-		cout << x << ", " << y << "\n";
+		antCam = Vec3f(camx, camy, camz);
+		// obtengo el punto de impacto en el espacio 3d antes de mover la cámara
+		float x = ((2.0f * (float)coor_x) / (float)width) - 1.0f;
+		if (x > 0) {
+			x /= 0.96f;
+			if (x > 1) x = 1;
+		}
+		float y = 1.0f - (((float)coor_y * 2) / (float)height);
+		if (y < 0) {
+			y /= 0.96f;
+			if (y < -1) y = -1;
+		}
+		float z = -1.0f;
+
+		float angx = tan(x/1.5f);
+		float angy = tan(y/1.5f);
+
+		cout << "camera " << x << ", " << y;
+		
+		Mat3f rotx = rotMatrix(upVect, -angx);
+		Mat3f roty = rotMatrix(rightVect, angy);
+		//Mat3f rotz = rotMatrix(rightVect, 180);
+		Mat3f rot = rotx * roty ;
+		Mat3f points(
+			backVect[0], 0.0f, 0.0f,
+			backVect[1], 0.0f, 0.0f,
+			backVect[2], 0.0f, 0.0f
+		);
+		Mat3f resp = rot * points;
+		Vec3f dir(-resp[0][0], -resp[1][0], -resp[2][0]);
+		dir.normalize();
+
+
+		col = dir;
+		// compruebo si colisiona con algún ojeto
+		int* collide = checkCollide(dir);
+		if (collide != NULL) {
+			cout << "chocó con x:" << collide[0] << ", y:" << collide[1] << ", z:" << collide[2] << "\n";
+			/*if (c.isEmpty(collide[0], collide[1], collide[2])) {
+				c.mark(currPlayer, collide[0], collide[1], collide[2]);
+			} else {
+				// TODO: anunciar que es una pieza ocupada
+			}*/
+		} else {
+			cout << "sin choque\n";
+		}
 	}
+	display();
+}
+
+
+int* checkCollide(Vec3f d) {
+	// usar las colisiones del proyecto 3
+	float collideDist = INFINITY;
+	int retPos[3];
+	for (int z = -1; z <= 1; z++) {
+		for (int y = -1; y <= 1; y++) {
+			for (int x = -1; x <= 1; x++) {
+				//cout << "\n\n\ntesteando " << x + 1 << " " << y + 1 << " " << z + 1 << "\n";
+				//cout << "x:" << x * 1.5 << " y:" << y * 1.5 << " z:" << z * 1.5 << "\n";
+				float collided;
+				Vec3f lo(camx - x * 1.5, camy - y * 1.5, camz - z * 1.5);
+				if (centerTest(x, y, z)) {
+					//cout << "box\n";
+					collided = collideBox(lo, d);
+				} else {
+					collided = collideSphere(lo, d);
+					//cout << "sphere\n";
+				}
+				if (collided >= 0 && collided < collideDist) {
+					collideDist = collided;
+					retPos[0] = x + 1;
+					retPos[1] = y + 1;
+					retPos[2] = z + 1;
+				}
+			}
+		}
+	}
+	if (collideDist != INFINITY) {
+		cout << "\nt: " << collideDist << "\n";
+		return retPos;
+	}
+	return NULL;
+
+}
+
+float collideSphere(Vec3f o, Vec3f d) {
+	float k, c, t0, t1, disc, ft;
+	unsigned x = 0, y = 1, z = 2;
+
+	k = o[x] * d[x] + o[y] * d[y] + o[z] * d[z];
+	c = o[x] * o[x] + o[y] * o[y] + o[z] * o[z] - 0.25;
+
+	disc = k * k - c;
+
+	if (disc < 0) {
+		return -1;
+	}
+
+	if (disc == 0.0) {
+		ft = -k + sqrt(disc);
+	}
+	else {
+		t0 = -k + sqrt(disc);
+		t1 = -k - sqrt(disc);
+		if (t0 >= 0 && t1 >= 0) {
+			if (t0 < t1) {
+				ft = t0;
+			}
+			else {
+				ft = t1;
+			}
+		}
+		else if (t0 >= 0) {
+			ft = t0;
+		}
+		else {
+			ft = t1;
+		}
+	}
+
+	Vec3f pos = o + d * ft;
+	cout << "l" << pos.length()<<"  ";
+	if (pos.length() > 0.5f) {
+		return -1;
+	}
+	return ft;
+}
+
+float collideBox(Vec3f o, Vec3f d) {
+	int it;
+	float x, y, t, bestT;
+	int mod0, mod1, mod2, bestIndex;
+
+	bestT = HUGE_DOUBLE;
+	bestIndex = -1;
+
+	for (it = 0; it < 6; it++) {
+		mod0 = it % 3;
+
+		if (d[mod0] == 0) {
+			continue;
+		}
+
+		t = ((it / 3) - 0.5 - o[mod0]) / d[mod0];
+
+		mod1 = (it + 1) % 3;
+		mod2 = (it + 2) % 3;
+		x = o[mod1] + t * d[mod1];
+		y = o[mod2] + t * d[mod2];
+
+		if (x <= 0.5 && x >= -0.5 &&
+			y <= 0.5 && y >= -0.5)
+		{
+			if (bestT > t) {
+				bestT = t;
+				bestIndex = it;
+			}
+		}
+
+	}
+
+	if (bestIndex < 0) return -1;
+
+	return bestT;
 }
 
 void mouseWheel(int button, int dir, int x, int y) {
-	if (dir > 0) {
-		zoom -= zoomSensibility;
-	}
-	else {
-		zoom += zoomSensibility;
-	}
+	float move = zoomSensibility * -dir;
+	zoom += move;
 
-	float sx = sin(angx);
-	float cx = cos(angx);
-	float sy = sin(angy);
-	float cy = cos(angy);
-
-	camx = zoom * (sy);
-	camy = zoom * (sx * cy);
-	camz = zoom * (cx * cy);
-
+	camx += move * backVect[0];
+	camy += move * backVect[1];
+	camz += move * backVect[2];
+	cout << "zoom " << zoom << "\n";
 	display();
 }
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(720, 640);
+	glutInitWindowSize(width, height);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_RGB);
 
 	glutCreateWindow("Proy 4 | Simon Esquivel | Martin Tamay");
@@ -152,56 +345,88 @@ void reshape(int w, int h) {
 	glMatrixMode(GL_MODELVIEW);//restaurar la matriz de modelo-vista como activa*/
 }
 
-void keyboard(unsigned char key, int x, int y) {
+Mat3f rotMatrix(Vec3f u, float ang) {
+	unsigned x = 0, y = 1, z = 2;
+	float c = cos(ang);
+	float s = sin(ang);
+	float t = tan(ang);
+
+
+	Mat3f rot(
+		c + pow(u[x], 2) * (1 - c), u[x] * u[y] * (1 - c) - u[z] * s, u[x] * u[z] * (1 - c) + u[y] * s,
+		u[y] * u[x] * (1 - c) + u[z] * s, c + pow(u[y], 2) * (1 - c), u[y] * u[z] * (1 - c) - u[x] * s,
+		u[z] * u[x] * (1 - c) - u[y] * s, u[z] * u[y] * (1 - c) + u[x] * s, c + pow(u[z], 2) * (1 - c)
+	);
+	return rot;
+}
+
+void keyboard(unsigned char key, int x_key, int y_key) {
+	int dir = 1;
+	Vec3f u, *v2, *v3;
 	switch (key) {
 		// para mover la cámara
+	case 'w':
+	case 'W':
+		u = rightVect;
+		v2 = &upVect;
+		v3 = &backVect;
+		break;
+	case 's':
+	case 'S':
+		dir = -1;
+		u = rightVect;
+		v2 = &upVect;
+		v3 = &backVect;
+		break;
 	case 'a':
 	case 'A':
-		angy -= camspeed;
+		u = upVect;
+		v2 = &rightVect;
+		v3 = &backVect;
 		break;
 	case 'd':
 	case 'D':
-		angy += camspeed;
-		break;
-
-	case 'w':
-	case 'W':
-		angx -= camspeed;
-		break;
-
-	case 's':
-	case 'S':
-		angx += camspeed;
+		dir = -1;
+		u = upVect;
+		v2 = &rightVect;
+		v3 = &backVect;
 		break;
 	case 'q':
 	case 'Q':
-		angz -= camspeed;
+		u = backVect;
+		v2 = &rightVect;
+		v3 = &upVect;
 		break;
 	case 'e':
 	case 'E':
-		angz += camspeed;
+		dir = -1;
+		u = backVect;
+		v2 = &rightVect;
+		v3 = &upVect;
 		break;
+	default:
+		return;
 	}
 
-	float sx = sin(angx);
-	float cx = cos(angx);
-	float sy = sin(angy);
-	float cy = cos(angy);
+	unsigned x = 0, y = 1, z = 2;
+	Mat3f rot = rotMatrix(u, camspeed * dir);
+	Mat3f points(
+		camx, (*v2)[x], (*v3)[x],
+		camy, (*v2)[y], (*v3)[y],
+		camz, (*v2)[z], (*v3)[z]
+	);
 
-	camx = zoom * (sy);
-	camy = zoom * (sx * cy);
-	camz = zoom * (cx * cy);
+	Mat3f rest = rot * points;
 
-	sx = sin(-angx);
-	cx = cos(-angx);
-	sy = sin(-angy);
-	cy = cos(-angy);
-	float sz = sin(-angz);
-	float cz = cos(-angz);
-
-	tx = -(sz * cy);
-	ty = -sx * sy * sz + cx * cz;
-	tz = cx * sy * sz + cz * sx;
+	camx = rest[0][0];
+	camy = rest[1][0];
+	camz = rest[2][0];
+	(*v2)[0] = rest[0][1];
+	(*v2)[1] = rest[1][1];
+	(*v2)[2] = rest[2][1];
+	(*v3)[0] = rest[0][2];
+	(*v3)[1] = rest[1][2];
+	(*v3)[2] = rest[2][2];
 
 	display();
 }
